2df8017267c517f0f94230f8cb3161680f685fc9
Mateusz Kondzior <m.kondzior@external.tme.eu>
2025-03-27 14:22:51 +0100

    add ssh-key ids list to output

--- modules/ssh-key/outputs.tf
+++ modules/ssh-key/outputs.tf
@@ -5,3 +5,7 @@ output "ssh_key_gen_id" {
 output "ssh_key_user_id" {
   value = digitalocean_ssh_key.ssh_key_user.id
 }
+
+output "ssh_key_list_ids" {
+  value = [digitalocean_ssh_key.ssh_key_gen.id, digitalocean_ssh_key.ssh_key_user.id]
+}
