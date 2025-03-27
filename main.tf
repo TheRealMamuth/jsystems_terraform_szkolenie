12469595c2def2709671ece0a94dea33bfdfeecd
kacper nowak <nowakkacper214@gmail.com>
2025-03-27 13:34:29 +0100

    update

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-module "firewall" {
-  source = "./modules/firewall_module_kn"
-}
-
 #module "waszanazwa" {
 #  source = "./modules/moj_modul"
 #
 #  arg_to_wasze_wariables = ""
 #
 #}
+
+
+module "firewall" {
+  source = "./modules/firewall_module_kn"
+}
