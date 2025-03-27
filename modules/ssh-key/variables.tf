8a880f1a48d0fea3daada4f1aaccbd532baad405
Mateusz Kondzior <m.kondzior@me.pl>
2025-03-27 13:52:32 +0100

    fix algorithm upper case

--- modules/ssh-key/variables.tf
+++ modules/ssh-key/variables.tf
@@ -1,10 +1,10 @@
 variable "ssh_algorithm" {
   type        = string
   description = "algorytm szyfrowania klucza ssh"
-  default     = "ed25519"
+  default     = "ED25519"
   validation {
-    condition     = var.ssh_algorithm == "ed25519"
-    error_message = "Algorytm musi byc ed25519"
+    condition     = var.ssh_algorithm == "ED25519"
+    error_message = "Algorytm musi byc ED25519"
   }
 
 }
