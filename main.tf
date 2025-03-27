8f3b3ce8a9d8b985b73ff41bd622ebe9d926f852
Arkadiusz Sieczak <sieczak.a@gmail.com>
2025-03-27 13:56:38 +0100

    feat: initial parameters for module project

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@
 #  arg_to_wasze_wariables = ""
 #
 #}
+
+module "project" {
+    source = "./modules/project"
+    project_name = "group_project"
+    description = "This is a group project"
+    environment = "Development"
+    project_resources = []
+}
