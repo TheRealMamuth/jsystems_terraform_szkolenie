249b5ee2ecd5767a1c8acfe4b008ccb1df598dad
Arkadiusz Sieczak <sieczak.a@gmail.com>
2025-03-27 14:17:45 +0100

    feat: initial project parameters (#10)
    * feat: initial parameters for module project
    
    * feat: initial parameters for module project #2
    
    ---------
    
    Co-authored-by: Piotr Kośka <piotr.s.koska@gmail.com>

--- main.tf
+++ main.tf
@@ -4,6 +4,16 @@
 #  arg_to_wasze_wariables = ""
 #
 #}
+
+module "project" {
+    source = "./modules/project"
+    project_name = "group_project"
+    description = "This is a group project"
+    environment = "Development"
+    project_resources = []
+
+}
+
 module "ssh-key" {
   source = "./modules/ssh-key"
 
