cefa58a59021f0b7e28c30323c00e874c549b36b
michalale <michalaleksandrowicz.dev@gmail.com>
2025-03-27 12:36:32 +0000

    rebase

---
+++ modules/moj_modul/variables.tf
@@ -0,0 +1,3 @@
+variable "arg_to_wasze_wariables" {
+  default = "wartosc"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "do_token" {
+  type        = string
+  description = "This is API for digitalocean"
+  sensitive   = true
+}
