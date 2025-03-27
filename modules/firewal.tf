25aeb993d5196cd5c54fb848c940499cf2ad1f12
kacper nowak <nowakkacper214@gmail.com>
2025-03-27 12:01:48 +0100

    FW config/kacper

---
+++ modules/firewal.tf
@@ -0,0 +1,34 @@
+resource "digitalocean_firewall" "web" {
+  name = "FW-all-out-22-in"
+
+  droplet_ids = [digitalocean_droplet.web.id]
+
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "0-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "0-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
