d8edba1240e96a416e29bfb7d8d21dd715d215c1
kacper nowak <nowakkacper214@gmail.com>
2025-03-27 14:28:56 +0100

    update

--- main.tf
+++ main.tf
@@ -7,20 +7,20 @@
 #
 #}
 
-module "firewall" {
-  source = "./modules/firewall_module_kn"
+module "project" {
+    source = "./modules/project"
+    project_name = "group_project"
+    description = "This is a group project"
+    environment = "Development"
+    project_resources = []
 }
-  
-#module "waszanazwa" {
-#  source = "./modules/moj_modul"
-#
-#  arg_to_wasze_wariables = ""
-#
-#}
+
 module "ssh-key" {
   source = "./modules/ssh-key"
 
+  # Argumety w module
   ssh_user_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIM3epJWkP6zxtbJcCaPTG+4UCklkJ7rb1uQ/9Xq7y0L"
+  ssh_algorithm       = "ED25519"
 }
 
 #module "waszanazwa" {
