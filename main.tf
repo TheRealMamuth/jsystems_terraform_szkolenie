e46cadee6c37c99dede6fc4adb8e5d54e5d05166
Piotr Koska <piotr.s.koska@gmail.com>
2025-03-27 14:18:17 +0100

    Update

--- main.tf
+++ main.tf
@@ -17,5 +17,7 @@ module "project" {
 module "ssh-key" {
   source = "./modules/ssh-key"
 
+  # Argumety w module
   ssh_user_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIM3epJWkP6zxtbJcCaPTG+4UCklkJ7rb1uQ/9Xq7y0L"
+  ssh_algorithm       = "ED25519"
 }
