925ebaba8a8961ce8961da0e4bcc251bd19f66a1
Mateusz Kondzior <m.kondzior@me.pl>
2025-03-27 13:37:02 +0100

    fix names

--- modules/ssh-key/main.tf
+++ modules/ssh-key/main.tf
@@ -1,21 +1,21 @@
 
 resource "random_string" "random_name" {
-  length = 5
+  length  = 5
   special = false
 }
 
-resource "tls_private_key" "ssh_key_gen" {
+resource "tls_private_key" "ssh_key_priv" {
   algorithm = arg.ssh_algorithm
 
 }
 
 
-resource "digitalocean_ssh_key" "key1" {
-   name       = "ssh-key-gen-${random_string.random_name.result}"
-  public_key = tls_private_key.ssh_key_gen.public_key_openssh
+resource "digitalocean_ssh_key" "ssh_key_gen" {
+  name       = "ssh-key-gen-${random_string.random_name.result}"
+  public_key = tls_private_key.ssh_key_priv.public_key_openssh
 }
 
-resource "digitalocean_ssh_key" "ssh_key" {
+resource "digitalocean_ssh_key" "ssh_key_user" {
   name       = "ssh-key-user-${random_string.random_name.result}"
   public_key = var.ssh_user_public_key
 }
