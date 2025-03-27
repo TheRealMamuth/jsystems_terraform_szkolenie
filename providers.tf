3388ef9f7661d7491d0da6e40576fb52875d94a5
Piotr Koska <piotr.s.koska@gmail.com>
2025-03-27 15:15:28 +0100

    Finalny Test

--- providers.tf
+++ providers.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = "eu-central-1"
+  access_key = var.ak
+  secret_key = var.sk
+  token = var.at
 }
 
 provider "digitalocean" {
--- variables.tf
+++ variables.tf
@@ -3,3 +3,23 @@ variable "do_token" {
   description = "This is API for digitalocean"
   sensitive   = true
 }
+
+variable "sk" {
+  type        = string
+  description = "This is secret key"
+  sensitive   = true
+  
+}
+
+variable "ak" {
+  type        = string
+  description = "This is access key"
+  sensitive   = true
+  
+}
+
+variable "at" {
+  type        = string
+  description = "This is token"
+  sensitive   = true
+}
