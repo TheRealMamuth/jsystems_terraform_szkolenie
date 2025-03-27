2f5dff49010ce43b68748478eb70c25a722ce371
DawidPawlowski123 <72730849+DawidPawlowski123@users.noreply.github.com>
2025-03-27 14:51:39 +0100

    Domain dp (#4)
    * Domain
    
    * Domain up
    
    * Update
    
    * Update
    
    * update
    
    * remove version
    
    * changes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: d.pawlowski <d.pawlowski@tme.pl>
    Co-authored-by: Piotr Koska <piotr.s.koska@gmail.com>

---
+++ modules/domain/main.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "main" {
+  name = var.zone_name
+}
+
+resource "aws_route53_record" "subdomain_records" {
+  for_each = var.droplet_ips
+
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "${each.key}.${var.zone_name}" 
+  type    = "A"
+  ttl     = 300
+  records = [each.value]
+}
+
+
