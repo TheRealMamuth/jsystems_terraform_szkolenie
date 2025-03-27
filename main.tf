75170f3683c17f1af355e2b77d0989389ea8b374
cuivier <cuivier.me@gmail.com>
2025-03-27 13:58:20 +0100

    Mateusz sshkey (#8)
    * add ssh-key module
    
    * add module to main .cf
    
    * fix names
    
    * fix fmt
    
    * add argument
    
    * fix alogrithm
    
    * fix algorithm upper case
    
    * change ex value to ssh key
    
    ---------
    
    Co-authored-by: Mateusz Kondzior <m.kondzior@me.pl>

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@
 #  arg_to_wasze_wariables = ""
 #
 #}
+module "ssh-key" {
+  source = "./modules/ssh-key"
+
+  ssh_user_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIM3epJWkP6zxtbJcCaPTG+4UCklkJ7rb1uQ/9Xq7y0L"
+}
